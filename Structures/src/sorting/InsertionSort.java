9b92f8ffa6737ec33b977cad100339cc3eeb008e
Lucas <Lucas@Lucas-PC>
2015-11-01 03:27:11 -0300

    commit

--- Structures/src/sorting/InsertionSort.java
+++ Structures/src/sorting/InsertionSort.java
@@ -1,15 +1,31 @@
 package sorting;
 
-public class InsertionSort<T> implements Sort<T> {
-
+public class InsertionSort<T extends Comparable<T>> implements Sort<T> {
+	
 	@Override
-	public void sort(T[] array, int left, int right) {
-		
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int j = leftIndex + 1 ; j < rightIndex+1; j++) {
+			T key = array[j];
+			int i = j -1 ;
+			while ((i >= leftIndex) && (array[i].compareTo(key)) > 0){
+				array[i+1] = array[i];
+				i--;
+			}
+			array[i+1] = key;
+		}		
 	}
 
 	@Override
 	public void sort(T[] array) {
-		
+		for (int j = 0 ; j < array.length; j++) {
+			T key = array[j];
+			int i = j -1 ;
+			while ((i >= j) && (array[i].compareTo(key)) > 0){
+				array[i+1] = array[i];
+				i--;
+			}
+			array[i+1] = key;
+		}		
 	}
 
 }
--- Structures/src/sorting/SelectionSort.java
+++ Structures/src/sorting/SelectionSort.java
@@ -1,11 +1,21 @@
 package sorting;
 
-public class SelectionSort<T> implements Sort<T> {
+public class SelectionSort<T extends Comparable<T>> implements Sort<T> {
 
 	@Override
-	public void sort(T[] array, int left, int right) {
-		
-	}
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i < rightIndex + 1; i++) {
+			int minimo = i;
+			for (int j = i+1; j <= rightIndex; j++) {
+				if(array[minimo].compareTo(array[j]) > 0)
+					minimo = j;
+			}
+			T temp = array[i];
+			array[i] = array[minimo];
+			array[minimo] = temp;
+			}
+		}
+	
 
 	@Override
 	public void sort(T[] array) {
